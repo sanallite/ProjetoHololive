6bb38da114ed4adb1688c2f9e8ac57187c156b93
Márcio Teodoro <149595409+sanallite@users.noreply.github.com>
2025-04-17 18:22:33 -0300

    Barra de progresso nas músicas

--- js/playerAudio.js
+++ js/playerAudio.js
@@ -9,16 +9,24 @@ const botaoPause = document.querySelector('button.pause');
 const botaoProxima = document.querySelector('button.proxima');
 const botaoAnterior = document.querySelector('button.anterior');
 const nomeMusica = document.querySelector('section#musicas p.nome');
+const barra = document.querySelector('section#musicas div.progresso');
+const barraProgresso = document.querySelector('section#musicas div.barraProgresso');
 
-let indiceAtual = 0;
+let indiceAtual = Number(sessionStorage.getItem('musicaAtual')) || 0;
 
 const tocar = () => {
     player.play();
     nomeMusica.textContent = musicas[indiceAtual].nome;
-}
+    botaoPlay.classList.add('hidden');
+    botaoPause.classList.remove('hidden');
 
+    sessionStorage.setItem('musicaAtual', indiceAtual);
+    console.log(sessionStorage.getItem('musicaAtual'));
+}
 const pausar = () => {
     player.pause();
+    botaoPause.classList.add('hidden');
+    botaoPlay.classList.remove('hidden');
 }
 
 const anterior = () => {
@@ -30,12 +38,9 @@ const anterior = () => {
         indiceAtual -= 1;
     }
 
-    player.pause();
-    nomeMusica.textContent = '';
-
+    pausar();
     player.setAttribute('src', musicas[indiceAtual].src);
-    player.play();
-    nomeMusica.textContent = musicas[indiceAtual].nome;
+    tocar();
 }
 
 const proxima = () => {
@@ -47,20 +52,53 @@ const proxima = () => {
         indiceAtual += 1;
     }
 
-    player.pause();
-    nomeMusica.textContent = '';
-
+    pausar();
     player.setAttribute('src', musicas[indiceAtual].src);
-    player.play();
-    nomeMusica.textContent = musicas[indiceAtual].nome;
+    tocar();
 }
 
 player.setAttribute('src', musicas[indiceAtual].src);
-player.volume = 0.3;
-
-tocar();
 
 botaoPlay.addEventListener('click', tocar);
 botaoPause.addEventListener('click', pausar);
 botaoAnterior.addEventListener('click', anterior);
 botaoProxima.addEventListener('click', proxima);
+
+player.addEventListener('loadedmetadata', () => {
+    player.volume = 0.3;
+    tocar();
+})
+
+player.addEventListener('ended', () => {
+    proxima();
+});
+
+player.addEventListener('timeupdate', () => {
+    if ( player.duration ) {
+        const porcentagem = (player.currentTime / player.duration) * 100;
+        barraProgresso.style.width = `${porcentagem}%`;
+    }
+});
+
+barra.addEventListener('click', event => {
+    const dimensoes = barra.getBoundingClientRect();
+    const larguraBarra = dimensoes.width;
+    const localClique = event.clientX - dimensoes.left;
+    const porcentagem = (localClique / larguraBarra);
+
+    player.currentTime = player.duration * porcentagem;
+});
+
+document.addEventListener('keydown', (event) => {
+    if ( event.code === 'Space' ) {
+        event.preventDefault();
+
+        if ( player.paused ) {
+            tocar();
+        }
+
+        else {
+            pausar();
+        }
+    }
+});
